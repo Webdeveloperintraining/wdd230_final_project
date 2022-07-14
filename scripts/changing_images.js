55c659fd605045703435a6bd11dd3e8814b653e8
Webdeveloperintraining <sil22006@byui.edu>
2022-07-13 18:37:18 -0700

    added content to pages

--- scripts/changing_images.js
+++ scripts/changing_images.js
@@ -3,7 +3,7 @@ const leftArrow=document.querySelector("#left")
 const rightArrow=document.querySelector("#right")
 
 var i=0;
-var images=["./images/bapstism-related-content-small.jpg","./images/preparing-to-enter-the-temple-small.jpg","./images/outside the temple-small.jpg","./images/temple-sealing-small.jpg"];
+var images=["./images/bountiful-temple-large.jpg","./images/bapstism-related-content-large.jpg","./images/calgary-alberta-temple-large.jpg","./images/preparing-to-enter-the-temple-large.jpg","./images/outside the temple-large.jpg","./images/temple-sealing-large.jpg"];
 var time=4000;
 
 function changingImages(){
