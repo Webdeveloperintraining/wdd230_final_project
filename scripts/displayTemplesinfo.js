a95afe050e364fc99d7c5d6b1c5086a0fa5c1e5f
Webdeveloperintraining <sil22006@byui.edu>
2022-07-16 11:43:30 -0700

    temples JSON & JavaScript complete

---
+++ scripts/displayTemplesinfo.js
@@ -0,0 +1,46 @@
+const requestURL = "https://webdeveloperintraining.github.io/wdd230_final_project/json/data.json";
+
+fetch(requestURL)
+.then(function (response){
+    return response.json();
+})
+.then(function (jsonObject){
+    const companies=jsonObject["temples"];
+    companies.forEach(templeInfo)
+});
+
+function templeInfo(temple){
+    let section=document.createElement("section");
+    let name=document.createElement("h2");
+    let phone=document.createElement("p");
+    let email=document.createElement("p");
+    let address=document.createElement("p");
+    let dedicated=document.createElement("p");
+    let ordinanceSchedule=document.createElement("p");
+    let sessionSchedule=document.createElement("p");
+    let closureSchedule=document.createElement("p");
+    let picture=document.createElement("img");
+
+    name.innerHTML=temple.templeName;
+    phone.innerHTML=`<b>Telephone:</b> ${temple.telephone}`;
+    address.innerHTML=`<b>Address:</b> <a href="${temple.addressLink}" target="_blank">${temple.address}</a>`;
+    dedicated.innerHTML=`<b>Dedicated:</b> ${temple.dedicated}`;
+    email.innerHTML=`<b>Email:</b> ${temple.email}`;
+    ordinanceSchedule.innerHTML=`<b>Ordinance schedule:</b> ${temple.membershipLevel}`;
+    sessionSchedule.innerHTML=`<b>Session Schedule:</b> ${temple.sessionSchedule}`;
+    closureSchedule.innerHTML=`<b>Closure Schedule</b> ${temple.closureSchedule}`;
+    picture.setAttribute("src",temple.image);
+    picture.setAttribute("alt",`${temple.templeName} temple`);
+
+    section.appendChild(name);
+    section.appendChild(picture);
+    section.appendChild(address);
+    section.appendChild(email)
+    section.appendChild(phone);
+    section.appendChild(dedicated);
+    section.appendChild(ordinanceSchedule);
+    section.appendChild(sessionSchedule);
+    section.appendChild(closureSchedule);
+
+    document.querySelector(".temples").appendChild(section);
+};
